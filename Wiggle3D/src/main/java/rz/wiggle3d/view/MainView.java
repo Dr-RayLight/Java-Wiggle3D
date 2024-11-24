ce10729494e10fcdb74e8083d071b2ae8b79adf9
Raymond's SourceTree <raymondzorrofinder@gmail.com>
2024-11-24 15:50:06 +0800

    Start to Implement detail

--- Wiggle3D/src/main/java/rz/wiggle3d/view/MainView.java
+++ Wiggle3D/src/main/java/rz/wiggle3d/view/MainView.java
@@ -5,8 +5,8 @@ import java.awt.*; // For BorderLayout and other layout managers
 
 public class MainView extends JFrame {
 
+    // Global Variables
     private static final double HEADER_DIVDER_RATIO = 0.1;
-    private static final double BODY_DIVIDER_RATIO = 0.35;
 
     private static final int BORDER_SPLITER_SIZE = 5;
     private static final int WIDTH = 1200;
@@ -14,6 +14,14 @@ public class MainView extends JFrame {
 
     private final static String TITLE = "Wiggle Stereoscopy Generator";
 
+    public enum RenderType {
+        ORIGIN, DEPTH_MAP, WIGGLE
+    }
+
+    public RenderType mRenderType = RenderType.ORIGIN;
+
+    // --------------------------------------------------------------------------
+    // Constructor
     public MainView() {
         // Basic setup.
         setTitle(TITLE);
@@ -21,13 +29,15 @@ public class MainView extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
-        
+
         // Layout Def.
         JSplitPane mainPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, HeaderView.build(), BodyView.build());
         mainPane.setDividerLocation(HEADER_DIVDER_RATIO);
         mainPane.setResizeWeight(HEADER_DIVDER_RATIO);
         mainPane.setDividerSize(BORDER_SPLITER_SIZE);
         add(mainPane);
-   
     }
+
+    // --------------------------------------------------------------------------
+
 }
---
+++ cleanGitCache.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+#To clear your entire Git cache, use the “git rm” command with the “-r” option for recursive.
+echo "Start to clear entire git caches."
+git rm -r --cached .
+
+
+# Reference: 
+#https://devconnected.com/how-to-clear-git-cache/#:~:text=To%20clear%20your%20entire%20Git,git%20rm%20%2Dr%20%2D%2Dcached%20.
